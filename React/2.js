ea544e6219c82803a674f14dd7ed4f0236950b57
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-14 05:02:44 +0300

    Create a Complex JSX Element

---
+++ React/2.js
@@ -0,0 +1,11 @@
+const JSX = <div>
+                <h1>Paragraph One</h1>
+                <p>Paragraph Two</p>
+                <ul>
+                  <li>1</li>
+                  <li>2</li>
+                  <li>3</li>
+                </ul>
+            </div>;
+
+console.log(JSX)
