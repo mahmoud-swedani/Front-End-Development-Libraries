a499c0fbd346a7ad230f3063bab71df5c15b6fa4
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-15 12:49:48 +0300

    Using Props with Stateless Functional Components

---
+++ React/20.js
@@ -0,0 +1,19 @@
+
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper name="Prem"/>
+      </div>
+    );
+  }
+};
+// change code below this line
+
+const Camper = (props) => <p>{props.name}</p>
+
+Camper.defaultProps = {name:'CamperBot'};
+Camper.propTypes = { name: PropTypes.string.isRequired }
