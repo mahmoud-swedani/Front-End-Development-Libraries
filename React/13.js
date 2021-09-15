52a07d96d7f66b54953deb89338dbeb8c911beba
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-15 06:48:09 +0300

    Write a React Component from Scratch

---
+++ React/13.js
@@ -0,0 +1,19 @@
+// Change code below this line
+class MyComponent  extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div id='challenge-node'>
+          <h1>My First React Component!</h1>
+          {/* Change code below this line */}
+  
+          {/* Change code above this line */}
+        </div>
+      );
+    }
+  };
+  
+  // Change code below this line
+  ReactDOM.render(<MyComponent  />,document.getElementById("challenge-node"))
