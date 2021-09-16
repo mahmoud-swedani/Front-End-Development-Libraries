f4165f20715ca41ce4e44205d5e18f509b363396
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-16 07:47:18 +0300

    Give Sibling Elements a Unique Key Attribute

---
+++ React/45.js
@@ -0,0 +1,22 @@
+const frontEndFrameworks = [
+    'React',
+    'Angular',
+    'Ember',
+    'Knockout',
+    'Backbone',
+    'Vue'
+  ];
+  
+  function Frameworks() {
+    const renderFrameworks = frontEndFrameworks.map(function(item){
+          return <li key={item}>{item}</li>;
+        });; // Change this line
+    return (
+      <div>
+        <h1>Popular Front End JavaScript Frameworks</h1>
+        <ul>
+          {renderFrameworks}
+        </ul>
+      </div>
+    );
+  };
