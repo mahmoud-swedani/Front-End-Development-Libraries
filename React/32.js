52b72283e5520c70740cda68cc976b731a9ad0f3
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-16 06:10:03 +0300

    Use the Lifecycle Method componentWillMount

---
+++ React/32.js
@@ -0,0 +1,15 @@
+
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+      // Change code below this line
+      console.log(" The componentWillMount() method is called before the render() method when a component is being mounted to the DOM")
+  
+      // Change code above this line
+    }
+    render() {
+      return <div />
+    }
+  };
