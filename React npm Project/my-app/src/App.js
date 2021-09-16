b3778ee05271a349efe157ea77515e2d58ade7ea
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-16 15:29:06 +0300

    first project react {heloo, world}

---
+++ React npm Project/my-app/src/App.js
@@ -0,0 +1,13 @@
+import Heloo from "./Component/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Heloo />
+      </header>
+    </div>
+  );
+}
+
+export default App;
