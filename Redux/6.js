87a74e18cb34bd77c619a90ca64c20736ad4c8a2
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-24 17:16:45 +0300

    Handle an Action in the Store

---
+++ Redux/6.js
@@ -0,0 +1,24 @@
+// Handle an Action in the Store
+
+const defaultState = {
+  login: false
+};
+
+const reducer = (state = defaultState, action) => {
+  // Change code below this line
+  switch(action.type){
+    case 'LOGIN':
+      return {...state, login: true};
+    default:
+      return state;
+  }
+  // Change code above this line
+};
+
+const store = Redux.createStore(reducer);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN',
+      }
+};
